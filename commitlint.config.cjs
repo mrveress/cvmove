d76de4066e3283bbf482192b4aa180b7507fe759
Adam Petrovich <veress.honaht@gmail.com>
2025-09-04 11:39:17 +0300

    chore: update release configuration and commit linting rules
    - Added .release-please-manifest.json for versioning.
    - Enhanced commitlint configuration to enforce maximum lengths for subject, body, and header.
    - Updated release-please configuration to remove deprecated versioning option.
    - Adjusted GitHub Actions workflow to use the correct action repository.
    
    These changes improve the release process and maintain consistency in commit messages.

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,1 +1,8 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-max-length': [2, 'always', 300],
+    'body-max-line-length': [2, 'always', 300],
+    'header-max-length': [2, 'always', 300],
+  },
+};
